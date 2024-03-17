0d81e302d389607f0ca68b132c5493586b064710
Greg-Owen <gobennett2002@gmail.com>
2024-03-17 13:14:42 -0700

    Updated Main.js: added two more hyperlinks to close #18

--- assign03/src/components/Main.js
+++ assign03/src/components/Main.js
@@ -19,12 +19,13 @@ const Main = () => {
       <div>
         <p>Im currently learning react by making this website whilst following this tutorial, please check it out it is very useful.</p>
         <iframe width="640" height="360" src="https://www.youtube.com/embed/w7ejDZ8SWv8" title="React JS Crash Course" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <br></br><a href="https://www.youtube.com/watch?v=w7ejDZ8SWv8&t=815s">Link to video</a>
         <p>I have a github profile where this page will be uploaded: <a href="https://github.com/Greg-Owen">My profile</a></p>
       </div>
       <div id="skills">
         <h1>What can I do?</h1>
         <p>
-          As I mentioned in the section above I'm currently learning how to use react JS. I am always trying to improve my abilities and broaden my skillset. You can find a more comprehensive list down below.
+          As I mentioned in the section above I'm currently learning how to use <a href="https://legacy.reactjs.org/tutorial/tutorial.html">react JS</a>. I am always trying to improve my abilities and broaden my skillset. You can find a more comprehensive list down below.
         </p>
         <h3>Technical Skills</h3><br></br>
         <strong>Programming Languages</strong>: Java, C++, JavaScript, Python<br></br>
@@ -42,8 +43,8 @@ const Main = () => {
         </ul>
         <h3>Current Projects<br></br></h3>
         <ul>
-          <li>I am currently working in a team of four on a project for this course. It is called PcBee, a pc 
-            parts search site which incorporates both ChatGPT and eBay API's.<br></br><a href="https://github.com/Team-Lorem-Ipsum/PCBee">PcBee</a></li>
+          <li>I am currently working in a team of four on a project for this course. It is called <a href="https://github.com/Team-Lorem-Ipsum/PCBee">PcBee</a>, a pc 
+            parts search site which incorporates both ChatGPT and eBay API's.</li>
           <li>I have also been working on and off on a personal multiplayer platformer game which I am building 
             from scratch. It has yet to be uploaded to github but here is a screenshot of it:<br></br>
             <img src={require('./Images/PlatformerPrototypeScreenShot.png')} height="40%" width="60%"></img></li>
@@ -56,7 +57,7 @@ const Main = () => {
         <p>
           I'm currently studying at <a href="https://langara.ca/">Langara College</a> where I am a second year computer science student. Before I 
           started studying computer science I took a year of applied science and engineering, so not only does my coursework include data structures and algorithms, 
-          computer languages, and programming; but also includes physics, mathematics, and business and project management. I expected to transfer a university after 
+          computer languages, and programming; but also includes physics, mathematics, and business & project management. I expected to transfer a university after 
           my second year is over and graduate in 2026. 
         </p>
       </div>
